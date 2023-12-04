e043050c0a6cfe2e5564e751dae38a55707bdd79
ac009527 <18571853917@163.com>
2023-12-04 10:03:57 +0800

    store type UseUserStoreType

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import './App.css'
-import './i18n'
-import { useTranslation } from 'react-i18next'
-import { useDemoStore } from './store'
-
+import "./i18n";
+import { useTranslation } from "react-i18next";
+import { useDemoStore } from "./store";
+import styles from "./App.module.scss";
 function App() {
-
-  const { count, inc } = useDemoStore()
-  const { t } = useTranslation()
+  const { count, inc } = useDemoStore();
+  const { t } = useTranslation();
   return (
     <>
-      <div> {t('hello')}</div>
-      react + ts + zustand + i18n + vite + ahook
+      <div> {t("hello")}</div>
+      <div className={styles.testStyles}>
+        react + ts + zustand + i18n + vite + ahook
+      </div>
 
       <div>
         demo
@@ -20,7 +20,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,52 +1,30 @@
-import { create, createStore } from 'zustand';
-import { devtools } from 'zustand/middleware';
-type UserInfoResult = Record<string, any>;
-export const defaultUserData = {
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+export type  UseUserStoreType = {
   userInfo: {
-    id: '',
-    phone: '',
-    nickname: '',
-    full_name: '',
-    balance: 0,
-  },
-  upcomingMessages: [] as UpcomingMessageData[],
-};
-export type UserStateType = typeof defaultUserData;
-export type UpcomingMessageData = {
-  e: number;
-  b?: Record<string, any>;
-};
-export interface Functions {
-  setUserInfo: (userInfo?: UserInfoResult) => void;
-  addUpcomingMessage(v: UpcomingMessageData): any;
-  clearUpcomingMessage(): void;
+    id: string;
+    phone: string;
+    nickname: string;
+    full_name: string;
+    balance: number;
+  };
+  setUserInfo: (userInfo?: UseUserStoreType['userInfo']) => void;
 }
-export const useDemoStore = create(devtools((set, _v) => ({
-    count: 1,
-    inc: () => set((state: any) => ({ count: state.count + 1 })),
-  })))
+export type UseDemoStoreType = {
+  count: number;
+  inc: () => void;
+};
 
-export const useUserStore = createStore<typeof defaultUserData & Functions>()(
-    devtools((set, v) => {
-      return {
-        ...defaultUserData,
-        setUserInfo: (userInfo?: UserInfoResult) => set({ userInfo } ),
-        addUpcomingMessage: (v: UpcomingMessageData) =>
-          set(
-            (state : typeof defaultUserData) =>
-              ({ upcomingMessages: state.upcomingMessages.concat([v]) }) 
-          ),
-        clearUpcomingMessage: () => set({ upcomingMessages: [] } ),
-        // 多次使用的全局状态写在这，简化内容
-        // async updateUserInfo() {
-        //   if (!getCookie('token')) return;
-        //   return userInfo().then((userInfo) => {
-        //     if (isObject(userInfo)) {
-        //       set({ userInfo } as any);
-        //     }
-        //     return userInfo;
-        //   });
-        // },
-      };
-    })
-  );
+export const useDemoStore = create<UseDemoStoreType>()(
+  devtools((set) => ({
+    count: 1,
+    inc: () => set((state) => ({ count: state.count + 1 })),
+  }))
+);
+
+export const useUserStore = create<UseUserStoreType>()(
+  devtools((set) => ({
+    ...defaultUserData,
+    setUserInfo: (userInfo?: UseUserStoreType['userInfo']) => set({ userInfo }),
+  }))
+);
